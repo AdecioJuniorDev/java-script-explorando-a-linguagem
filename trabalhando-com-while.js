71c3ed7892c3a49cf62fe78054bf82f768de4875
Adecio Júnior <adeciojr88@hotmail.com>
2023-02-14 00:51:36 -0300

    first commit

---
+++ trabalhando-com-while.js
@@ -0,0 +1,9 @@
+let contador = 0
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe") 
+    }else{ 
+        console.log("Destino não existe");
+    }
+    contador += 1;
+} 
